07516c2c6de9ed24944c166d4c3df39eee0a03df
Federico Costa <federicojoaquincosta@gmail.com>
2022-04-21 10:53:21 -0300

    variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+var numeroDiscos = 5;
+var cuadras = 4
+var nombreGato = "Wolfgang";
+var nombrePerra = "Megumi";
+var estaSoleado = true;
+var hayParo = false;
+var pi = 3.14;
+var sensacionTermica = 16.8;
+
+
+document.write(`
+<h4> número de discos: ${ numeroDiscos }</h4>
+<h4> cuadras: ${ cuadras }</h4>
+<h4> nombre de mi gato: ${ nombreGato }</h4>
+<h4> nombre de mi perra: ${ nombrePerra }</h4>
+<h4> esta soleado?: ${ estaSoleado}</h4>
+<h4> hya paro?: ${ hayParo }</h4>
+<h4> valor de pi: ${ pi }</h4>
+<h4> sensación térmica: ${ sensacionTermica }</h4>
+`)
