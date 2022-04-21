15b6e2e4660515ef0cd544c720116c45f12c52ed
Federico Costa <federicojoaquincosta@gmail.com>
2022-04-21 11:04:50 -0300

    arregle cositas

--- variables.js
+++ variables.js
@@ -7,6 +7,15 @@ var hayParo = false;
 var pi = 3.14;
 var sensacionTermica = 16.8;
 
+console.log( numeroDiscos );
+console.log( cuadras );
+console.log( nombreGato );
+console.log( nombrePerra );
+console.log( estaSoleado );
+console.log( hayParo );
+console.log( pi );
+console.log( sensacionTermica );
+
 
 document.write(`
 <h4> número de discos: ${ numeroDiscos }</h4>
@@ -14,7 +23,7 @@ document.write(`
 <h4> nombre de mi gato: ${ nombreGato }</h4>
 <h4> nombre de mi perra: ${ nombrePerra }</h4>
 <h4> esta soleado?: ${ estaSoleado}</h4>
-<h4> hya paro?: ${ hayParo }</h4>
+<h4> hay paro?: ${ hayParo }</h4>
 <h4> valor de pi: ${ pi }</h4>
 <h4> sensación térmica: ${ sensacionTermica }</h4>
 `)
